60af583172371a9d08efb345172a5249bbc392a3
Luciano Silva <luciano_jet@hotmail.com>
2023-11-06 15:46:40 -0300

    finalizado

--- js/api.js
+++ js/api.js
@@ -1,85 +1,168 @@
-// Função para buscar dados da API e criar os cards
-async function fetchAndDisplayCards() {
-    try {
-        const response = await fetch("https://64d2a382f8d60b1743624173.mockapi.io/noticias");
-        const data = await response.json();
-        const cardContainer = document.getElementById('card-container');
-        
-        // Iterar sobre os dados e criar os cards
-        data.forEach(conteudo => {
-            const card = document.createElement('div');
-            card.classList.add('col-md-6', 'col-md-6', 'mb-4'); // Classes do Bootstrap
-            card.innerHTML = `
-            <div class="col">
-            <div class="card efeitocard">
-              <div class="card-body backgroud ">
-                <h3 class="card-title text-center">${conteudo.titulo}</h5>
-                <p class="card-text mt-5 text-center lead">${conteudo.resumo}</p>
-              </div>
-              <ul class="list-group list-group-flush">
-              <h5 class="list-group-item">${conteudo.secao}</h5>
-              <li class="list-group-item">${conteudo.autor}</li>
-              <li class="list-group-item">${conteudo.data}</li>
-            </ul>
-            </div>
-          </div>
-            `;
-            cardContainer.appendChild(card);
-        });
-    } catch (error) {
-        console.error('Erro ao buscar dados da API:', error);
-    }
-}
+document.addEventListener("DOMContentLoaded", function () {
+  const noticiasPorTemaDiv = document.getElementById("noticiasPorTema");
 
-// Chamar a função para exibir os cards quando a página carregar
-window.onload = fetchAndDisplayCards;
+  // URL da API de notícias
+  const apiUrl = "https://64d2a382f8d60b1743624173.mockapi.io/noticias";
 
+  // Função para buscar notícias da seção "Ciência"
+  function buscarNoticias() {
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((noticias) => {
+        const noticiasCiencia = noticias.filter((noticia) => noticia.secao === "Ciência");
 
+        noticiasPorTemaDiv.innerHTML = "";
 
+        if (noticiasCiencia.length === 0) {
+          noticiasPorTemaDiv.innerHTML = "<p>Nenhuma notícia encontrada para este tema.</p>";
+        } else {
+          noticiasCiencia.forEach((noticia) => {
+            const noticiaDiv = document.createElement("div");
+            noticiaDiv.innerHTML = `
+                      <div class="container col-10 my-5">
+                          <h3 class="fw-bolder mb-4">${noticia.titulo}</h3>
+                          <p class="lead">${noticia.resumo}</p>
+                          <small class="me-3 rounded ">Autor: ${noticia.autor}</small>
+                          <small>Data: ${noticia.data}</small>
+                      </div>
+                      `;
+            noticiasPorTemaDiv.appendChild(noticiaDiv);
+          });
+        }
+      })
+      .catch((error) => {
+        console.error("Erro ao buscar notícias: " + error);
+      });
+  }
 
-
-
+  buscarNoticias();
+});
 
 
+// ----------------------------------------- ESPORTE
 
 
+document.addEventListener("DOMContentLoaded", function () {
+  const noticiasPorTemaDiv = document.getElementById("noticiasPorTema2");
+
+  // URL da API de notícias
+  const apiUrl = "https://64d2a382f8d60b1743624173.mockapi.io/noticias";
+
+  // Função para buscar notícias da seção "Ciência"
+  function buscarNoticias() {
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((noticias) => {
+        const noticiasCiencia = noticias.filter((noticia) => noticia.secao === "Esporte");
+
+        noticiasPorTemaDiv.innerHTML = "";
+
+        if (noticiasCiencia.length === 0) {
+          noticiasPorTemaDiv.innerHTML = "<p>Nenhuma notícia encontrada para este tema.</p>";
+        } else {
+          noticiasCiencia.forEach((noticia) => {
+            const noticiaDiv = document.createElement("div");
+            noticiaDiv.innerHTML = `
+                      <div class="container col-10 my-5">
+                          <h3 class="fw-bolder mb-4">${noticia.titulo}</h3>
+                          <p class="lead">${noticia.resumo}</p>
+                          <small class="me-3 rounded ">Autor: ${noticia.autor}</small>
+                          <small>Data: ${noticia.data}</small>
+                      </div>
+                      `;
+            noticiasPorTemaDiv.appendChild(noticiaDiv);
+          });
+        }
+      })
+      .catch((error) => {
+        console.error("Erro ao buscar notícias: " + error);
+      });
+  }
+
+  buscarNoticias();
+});
+
+// ------------------------------- SAUDE
+
+document.addEventListener("DOMContentLoaded", function () {
+  const noticiasPorTemaDiv = document.getElementById("noticiasPorTema3");
+
+  // URL da API de notícias
+  const apiUrl = "https://64d2a382f8d60b1743624173.mockapi.io/noticias";
+
+  // Função para buscar notícias da seção "Ciência"
+  function buscarNoticias() {
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((noticias) => {
+        const noticiasCiencia = noticias.filter((noticia) => noticia.secao === "Saúde");
+
+        noticiasPorTemaDiv.innerHTML = "";
+
+        if (noticiasCiencia.length === 0) {
+          noticiasPorTemaDiv.innerHTML = "<p>Nenhuma notícia encontrada para este tema.</p>";
+        } else {
+          noticiasCiencia.forEach((noticia) => {
+            const noticiaDiv = document.createElement("div");
+            noticiaDiv.innerHTML = `
+                      <div class="container col-10 my-5">
+                          <h3 class="fw-bolder mb-4">${noticia.titulo}</h3>
+                          <p class="lead">${noticia.resumo}</p>
+                          <small class="me-3 rounded ">Autor: ${noticia.autor}</small>
+                          <small>Data: ${noticia.data}</small>
+                      </div>
+                      `;
+            noticiasPorTemaDiv.appendChild(noticiaDiv);
+          });
+        }
+      })
+      .catch((error) => {
+        console.error("Erro ao buscar notícias: " + error);
+      });
+  }
+
+  buscarNoticias();
+});
 
 
+// --------------------- TECNOLOGIA
 
+document.addEventListener("DOMContentLoaded", function () {
+  const noticiasPorTemaDiv = document.getElementById("noticiasPorTema");
 
-// let noticia = document.querySelector("#noticia")
+  // URL da API de notícias
+  const apiUrl = "https://64d2a382f8d60b1743624173.mockapi.io/noticias";
 
-// async function carregarDados(){
-//     const url = "https://64d2a382f8d60b1743624173.mockapi.io/noticias"
+  // Função para buscar notícias da seção "Ciência"
+  function buscarNoticias() {
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((noticias) => {
+        const noticiasCiencia = noticias.filter((noticia) => noticia.secao === "Tecnologia");
 
-//     const resultado = await fetch(url)
-//     const dados = await resultado.json()
-//     console.log (dados)
-//     for (const item of dados){
+        noticiasPorTemaDiv.innerHTML = "";
 
-//         let card = document.createElement("div")
-//         const titulo = document.createElement("h1")
-//         const resumo = document.createElement("p")
-//         const autor = document.createElement("p")
-//         const secao = document.createElement("h3")
-//         const data = document.createElement("p")
+        if (noticiasCiencia.length === 0) {
+          noticiasPorTemaDiv.innerHTML = "<p>Nenhuma notícia encontrada para este tema.</p>";
+        } else {
+          noticiasCiencia.forEach((noticia) => {
+            const noticiaDiv = document.createElement("div");
+            noticiaDiv.innerHTML = `
+                      <div class="container col-10 my-5 shadow p-3 rounded-bottom-4">
+                          <h3 class="fw-bolder mb-4">${noticia.titulo}</h3>
+                          <p class="lead">${noticia.resumo}</p>
+                          <small class="me-3 rounded ">Autor: ${noticia.autor}</small>
+                          <small>Data: ${noticia.data}</small>
+                      </div>
+                      `;
+            noticiasPorTemaDiv.appendChild(noticiaDiv);
+          });
+        }
+      })
+      .catch((error) => {
+        console.error("Erro ao buscar notícias: " + error);
+      });
+  }
 
-//         titulo.textContent = item.titulo
-//         resumo.textContent = item.resumo
-//         autor.textContent = item.autor
-//         secao.textContent = item.secao
-//         data.textContent = item.data
-
-
-
-//         card.appendChild(titulo)
-//         card.appendChild(resumo)
-//         card.appendChild(autor)
-//         card.appendChild(secao)
-//         card.appendChild(data)
-
-//         noticia.appendChild(card)
-
-//     }
-// }
-// carregarDados()
+  buscarNoticias();
+});
